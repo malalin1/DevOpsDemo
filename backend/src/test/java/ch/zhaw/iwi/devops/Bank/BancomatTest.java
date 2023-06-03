b7ffaba144ddfb5a08aaeea8233af8512eca91de
malalin1 <92686066+malalin1@users.noreply.github.com>
2023-06-03 18:30:32 +0200

    add_Korrektur

--- backend/src/test/java/ch/zhaw/iwi/devops/Bank/BancomatTest.java
+++ backend/src/test/java/ch/zhaw/iwi/devops/Bank/BancomatTest.java
@@ -11,34 +11,41 @@ public class BancomatTest {
     public void setUp() {
         bancomat = new Bancomat(); // Erstellt ein neues Bancomat-Objekt vor jedem Testfall
     }
+
     @Test
     public void testInitialBalance() {
         assertEquals(0, bancomat.getBalance()); // Überprüft, ob die anfängliche Kontobalance 0 ist
     }
+
     @Test
     public void testDeposit() {
         bancomat.deposit(100); // Führt eine Einzahlung von 100 durch
         assertEquals(100, bancomat.getBalance()); // Überprüft, ob die Kontobalance nach der Einzahlung 100 ist
     }
+
     @Test
     public void testWithdrawSufficientFunds() {
         bancomat.deposit(100); // Führt eine Einzahlung von 100 durch
         bancomat.withdraw(50); // Führt eine Abhebung von 50 durch
         assertEquals(50, bancomat.getBalance()); // Überprüft, ob die Kontobalance nach der Abhebung 50 ist
     }
+
     @Test
     public void testWithdrawInsufficientFunds() {
         bancomat.deposit(100); // Führt eine Einzahlung von 100 durch
         bancomat.withdraw(150); // Führt eine Abhebung von 150 durch (mehr als der Kontostand)
+        assertEquals(100, bancomat.getBalance()); // Überprüft, ob die Kontobalance unverändert bleibt (keine Abhebung
+                                                  // erfolgt)
+    }
+
+    @Test
+    public void testSomething() {
+    }
+
+    @Test
+    public void testWithdrawNegativeAmount() {
+        bancomat.deposit(100); // Führt eine Einzahlung von 100 durch
+        bancomat.withdraw(-50); // Versucht eine negative Betrag abzuheben
         assertEquals(100, bancomat.getBalance()); // Überprüft, ob die Kontobalance unverändert bleibt (keine Abhebung erfolgt)
     }
-    @Test
-    public void testSomething() { 
-    }
-    @Test
-public void testWithdrawNegativeAmount() {
-    bancomat.deposit(100); // Führt eine Einzahlung von 100 durch
-    bancomat.withdraw(-50); // Versucht eine negative Betrag abzuheben
-    assertEquals(100, bancomat.getBalance()); // Überprüft, ob die Kontobalance unverändert bleibt (keine Abhebung erfolgt)
-}
 }
